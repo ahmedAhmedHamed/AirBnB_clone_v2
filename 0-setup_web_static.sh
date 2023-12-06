5227f191355ccda0225965036cdb5c195e3d847e
ahmedAhmedHamed <ahmedAhmedHamed0@gmail.com>
2023-12-06 20:31:32 +0200

    0 solved

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# chown -R ubuntu:ubuntu /data/
+touch /etc/nginx/conf.d/prepare_your_web_servers.conf
+echo 'server {
+    listen 80;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+}' > /etc/nginx/conf.d/prepare_your_web_servers.conf
+
