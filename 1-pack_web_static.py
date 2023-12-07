bba4128f2281f08b3c740779dd491c2fa7b035dc
ahmedAhmedHamed <ahmedAhmedHamed0@gmail.com>
2023-12-07 02:05:24 +0200

    added print to do_pack (1)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,11 @@ def do_pack():
     packs web_static into versions folder
     Return: the path of the saved targz
     """
+    time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    packpath = './versions/web_static_' + str(time)
+    print('Packing web_static to ' + packpath)
     if not os.path.exists('versions'):
         os.mkdir('versions')
-    time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    with tarfile.open('./versions/web_static_' + str(time), "w:gz") as tar:
+    with tarfile.open(packpath, "w:gz") as tar:
         tar.add('web_static', arcname=os.path.basename('static'))
-    return './versions/web_static_' + str(time)
+    return packpath
