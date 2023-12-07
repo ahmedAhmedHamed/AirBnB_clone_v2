bc4260acbfb727ef8783688a40de1c1117b7ac0a
ahmedAhmedHamed <ahmedAhmedHamed0@gmail.com>
2023-12-07 02:11:26 +0200

    if webstatic doesn't exist returns None (1)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,6 +14,8 @@ def do_pack():
     print('Packing web_static to ' + packpath)
     if not os.path.exists('versions'):
         os.mkdir('versions')
+    if not os.path.exists('web_static'):
+        return None
     try:
         with tarfile.open(packpath, "w:gz") as tar:
             tar.add('web_static', arcname=os.path.basename('static'))
