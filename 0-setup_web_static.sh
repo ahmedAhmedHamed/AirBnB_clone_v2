eba51cfefef8f690dca50822eb0df32cf997f25a
ahmedAhmedHamed <ahmedahmedhamedahmed0@gmail.com>
2023-12-09 13:36:08 +0200

    added install for nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # this script sets up a webserver using nginx.
+apt update
+apt install nginx
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
@@ -12,7 +14,7 @@ echo '<html>
   </body>
 </html>' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+# chown -R ubuntu:ubuntu /data/
 touch /etc/nginx/conf.d/prepare_your_web_servers.conf
 echo 'server {
     listen 80;
