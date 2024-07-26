8891e0479d2b24f0ef17105de5464aeb4243ce2c
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-26 11:42:37 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+houses the hello route solution
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    the / route that prints hello hbnb!
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_only():
+    """
+    the / route that prints HBNB!
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """
+    this is the main that runs the app
+    """
+    app.run(host='0.0.0.0', port='5000', debug=False)
