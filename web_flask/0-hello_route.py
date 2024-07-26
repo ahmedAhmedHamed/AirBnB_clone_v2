5f841ae4a2fc8fee0718a4895b2f394f34bf883c
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-26 23:04:57 +0300

    rollback

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+houses the hello route solution
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """
+    the / route that prints hello hbnb!
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
+    """
+    this is the main that runs the app
+    """
     app.run(host='0.0.0.0', port='5000', debug=False)
