a3795c4fe58dee3b8c48a3d4b07f1b9499295940
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-26 11:50:00 +0300

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+houses the hello route solution
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    the / route that prints hello hbnb!
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_only():
+    """
+    the / route that prints HBNB!
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    the / route that prints c + variable !
+    """
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    """
+    this is the main that runs the app
+    """
+    app.run(host='0.0.0.0', port='5000', debug=False)
