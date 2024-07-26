e2a2e80eedb8c676cb5ede619087d148e2494340
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-26 11:07:05 +0300

    requirements

--- web_flask/0-hellor_route.py
+++ web_flask/0-hellor_route.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+"""simply a hello_world!"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """the / route that prints hello hbnb!"""
     return "Hello HBNB!"
