c8107d15dcd891ab80f3a375180f664ae0b856b7
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-27 08:33:38 +0300

    tabs instead of spaces

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    """
-    the / route that prints hello hbnb!
-    """
-    return "Hello HBNB!"
+	"""
+	the / route that prints hello hbnb!
+	"""
+	return "Hello HBNB!"
 
 if __name__ == '__main__':
-    """
-    this is the main that runs the app
-    """
-    app.run(host='0.0.0.0', port='5000', debug=False)
+	"""
+	this is the main that runs the app
+	"""
+	app.run(host='0.0.0.0', port='5000', debug=False)
