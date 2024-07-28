51dfe669a6824e6ab5587c2a728747406134878d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-28 05:01:11 +0300

    teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -80,10 +80,9 @@ def states_list():
 	return render_template('7-states_list.html', states=states)
 
 
-# @app.teardown_appcontext
-# def teardown():
-# 	print('amogus')
-	# storage.close()
+@app.teardown_appcontext
+def teardown(e):
+	storage.close()
 
 
 if __name__ == '__main__':
